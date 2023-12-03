1ce8973c481cbcce769d336b21cb1e7ecf6b0b57
Görkem <Gkrem33@gmail.com>
2023-12-03 21:17:44 +0300

    ActivityType and Collection added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,38 @@
-import { Client } from "discord.js";
+import { ActivityType, Client, Collection } from "discord.js";
+import "dotenv/config";
+
 const client = new Client({
   intents: ["Guilds"],
+  presence: {
+    status: "idle",
+    // Ne yaptığını gösterir
+    activities: [{ name: "Breaking Bad", type: ActivityType.Watching }],
+  },
 });
-// "DIRECT_MESSAGES", , "GUILD_MEMBERS", "GUILD_MESSAGES"
+// "DIRECT_MESSAGES", , "GUILD_MEMBERS", "GUILD_MESSAGES" , "GuildPresences"
 client.on("ready", () => {
   console.log("Test bot hazır!!!");
-});
+  const collection = new Collection();
+  collection.set("a", 1);
+  collection.set("b", 2);
 
-client.login(
-  "MTE4MDgxOTg0NzIxOTc3MzU2MQ.GUW8Ml.FTmgsKzinsr0wgETOEKxnsXpbO5mhXVMlQ88uY"
-);
+  console.log(collection);
+
+  // client.user.setStatus("idle");
+
+  // const arr = [
+  //   "Better Call Saul",
+  //   "One Piece ",
+  //   "Breaking Bad ",
+  //   "Hunter X Hunter",
+  // ];
+
+  // setInterval(() => {
+  //   const random = Math.floor(Math.random() * arr.length);
+  //   client.user.setPresence({
+  //     activities: [{ name: arr[random], type: ActivityType.Watching }],
+  //   });
+  // }, 10000);
+});
+// Env dosyasından Tokeni alıp giriş işlemini yapar
+client.login(process.env.TOKEN);
