e97b289c1b4107e1bc1cd5036735ed352a516af8
Görkem <Gkrem33@gmail.com>
2023-12-05 15:08:53 +0300

    Embed added

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,5 +1,24 @@
+import { EmbedBuilder } from "discord.js";
+
 export default (client) => {
   client.on("messageCreate", (message) => {
-    console.log(message);
+    if (message.content == "Bilgi") {
+      const response = new EmbedBuilder()
+        .setTitle("Görkem Yazıcı")
+        .setAuthor({
+          name: "Software Developer",
+          iconURL:
+            "https://yt3.googleusercontent.com/ytc/APkrFKY_6HgdJPs74YbihU4h1nMKlypdpV_ocT8Vq8cFRg=s900-c-k-c0x00ffffff-no-rj",
+          url: "https://www.youtube.com/watch?v=A23xx0O1jso",
+        })
+        .setDescription(
+          "Görkem Yazıcı tarafından oluşturulmuş ne olduğu belirsiz bir bot"
+        )
+        .setColor("Gold")
+        .setFooter({ text: "Saat" })
+        .setTimestamp();
+
+      message.channel.send({ content: "Test Bot", embeds: [response] });
+    }
   });
 };
