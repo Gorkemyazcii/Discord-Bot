55f4518ed55ff38ecf3c1132b123ea7bb604d3a9
Görkem <Gkrem33@gmail.com>
2023-12-06 13:33:01 +0300

    cooldown control in development

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,3 +1,4 @@
+import cooldown_control from "../utils/cooldown_control.js";
 import { EmbedBuilder } from "discord.js";
 
 const prefix = process.env.prefix;
@@ -6,11 +7,15 @@ export default (client) => {
   client.on("messageCreate", (message) => {
     if (message.content.startsWith(prefix) == false) return;
 
-    const args = message.content.slice(1).trim().split(" ");
+    const args = message.content.slice(1).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
 
     const command = client.commands.get(commandName);
     if (!command) return;
+
+    // Cooldown control
+    const cooldown = cooldown_control();
+    if (cooldown) return console.log("cooldown var!!");
 
     try {
       command.execute(message);
