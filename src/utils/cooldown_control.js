ee5d60f489557bbeedd0270b004a7afa446b945e
Görkem <Gkrem33@gmail.com>
2023-12-07 14:40:56 +0300

    Cooldown added

--- src/utils/cooldown_control.js
+++ src/utils/cooldown_control.js
@@ -1,7 +1,47 @@
 import { Collection } from "discord.js";
 const cooldowns = new Collection();
 
-export default (message) => {
+export default (command, user_id) => {
+  if (user_id == process.env.USER_ID) return false;
+
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Collection());
+  }
+
   const now = Date.now();
-  cons;
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 5) * 1000;
+
+  if (timestamps.has(user_id)) {
+    const expiration = timestamps.get(user_id) + cooldownAmount;
+    if (now < expiration) {
+      const timeLeft = Math.round((expiration - now) / 1000);
+      return timeLeft;
+    }
+
+    return false;
+  } else {
+    timestamps.set(user_id, now);
+    setTimeout(() => {
+      timestamps.delete(user_id);
+    }, cooldownAmount);
+    return false;
+  }
 };
+
+/*
+cooldowns{
+    "ping":{
+        "819664166616956951" : Date.now()
+
+    },
+    "ban":{
+
+    }
+}
+
+setTimeout(()=>{
+    delete timestamps.delete(user_id)
+},5000)
+
+ */
