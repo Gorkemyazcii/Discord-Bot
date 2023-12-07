c397e3884fcc113a01a405ba09c4b023cdb34569
Görkem <Gkrem33@gmail.com>
2023-12-07 22:51:48 +0300

    embed added fixed

--- src/commands/Bot/ping.js
+++ src/commands/Bot/ping.js
@@ -3,15 +3,24 @@ import { EmbedBuilder } from "discord.js";
 export default {
   name: "ping",
   execute(message) {
-    const dicord_ping = message.client.ws.ping;
+    const { emoji, ws } = message.client;
+
+    const dicord_ping = ws.ping;
     const bot_ping = Date.now() - message.createdTimestamp;
 
-    const response = new EmbedBuilder()
-      .setColor("Random")
-      .addFields(
-        { name: "Discord ping", value: `${dicord_ping} ms`, inline: true },
-        { name: "Bot ping", value: `${bot_ping} ms`, inline: true }
-      );
+    const response = new EmbedBuilder().setColor("Random").addFields(
+      {
+        name: `${emoji("discord_ico74")} Discord ping `,
+        value: `${dicord_ping} ms `,
+        inline: true,
+      },
+      { name: "\u200b", value: "\u200b", inline: true },
+      {
+        name: `${emoji("BOT")} Bot ping`,
+        value: `${bot_ping} ms`,
+        inline: true,
+      }
+    );
     message.reply({ embeds: [response] });
   },
 };
