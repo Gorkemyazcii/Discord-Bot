d1f0fff3cbd90b19eb5e698a816400ea9376c7a9
Görkem <Gkrem33@gmail.com>
2023-12-07 22:51:23 +0300

    new embed and emoji

--- src/app.js
+++ src/app.js
@@ -4,13 +4,29 @@ import { readdirSync } from "fs";
 import "dotenv/config";
 
 const client = new Client({
-  intents: ["Guilds", "GuildMessages", "MessageContent", "GuildBans"],
+  intents: [
+    "Guilds",
+    "GuildMessages",
+    "MessageContent",
+    "GuildBans",
+    "GuildMembers",
+  ],
   presence: {
     status: "idle",
 
     activities: [{ name: "Breaking Bad", type: ActivityType.Watching }],
   },
 });
+// Command Loader
+client.commands = new Collection();
+// Assignments emoji
+client.emoji = (emojiName) =>
+  client.guilds.cache
+    .get(process.env.GUILD_ID)
+    .emojis.cache.find((e) => e.name == emojiName) || "❤️";
+//  Assignments Embed
+client.embed = await import("./utils/bot/embed.js").then((m) => m.default);
+
 // Event Loader
 readdirSync("./events").forEach(async (file) => {
   const event = await import(`./events/${file}`).then((m) => m.default); // m --> Module
@@ -18,8 +34,6 @@ readdirSync("./events").forEach(async (file) => {
 });
 
 // Command Loader
-client.commands = new Collection();
-
 readdirSync("./commands").forEach((category) => {
   readdirSync(`./commands/${category}`).forEach(async (file) => {
     const command = await import(`./commands/${category}/${file}`).then(
