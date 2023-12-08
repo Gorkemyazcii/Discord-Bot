20b6c792735c4fae62038701471e880d967e0e81
Görkem <Gkrem33@gmail.com>
2023-12-09 00:07:25 +0300

    SlashCommandBuilder added and fix

--- src/commands/Moderation/ban.js
+++ src/commands/Moderation/ban.js
@@ -1,9 +1,17 @@
-export default {
+import { EmbedBuilder } from "discord.js";
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+export const data = {
   name: "ban",
+  description: "Adam olmayanları banlar",
   permissions: "BanMembers",
   cooldown: 10,
-  execute(message) {
-    const { embed } = message.client;
-    message.reply({ embeds: [embed("Kullanıcı banlandı!!")] });
+  execute(interaction) {
+    const { embed } = interaction.client;
+    interaction.reply({ embeds: [embed("Kullanıcı banlandı!!")] });
   },
 };
+
+export const slash_data = new SlashCommandBuilder()
+  .setName(data.name)
+  .setDescription(data.description);
