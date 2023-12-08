0cf7c3d46835a990373bbad9d907e514435a05c4
Görkem <Gkrem33@gmail.com>
2023-12-09 00:08:40 +0300

    new ready

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,5 +1,9 @@
+import check_commands from "../utils/bot/check_commands.js";
+
 export default (client) => {
   client.once("ready", () => {
-    console.log("Bot hazır");
+    console.log("Test Bot hazır");
+
+    check_commands(client);
   });
 };
