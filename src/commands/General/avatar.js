871c4e41baa77d26da1aba7046c5c8c503729a8d
Görkem <Gkrem33@gmail.com>
2023-12-10 20:12:01 +0300

    Sunucu içerisinde kullanıcı adı girilen kişinin Resmini döndüren komut

--- src/commands/General/avatar.js
+++ src/commands/General/avatar.js
@@ -1,21 +1,47 @@
 import { EmbedBuilder } from "discord.js";
-import { SlashCommandBuilder } from "@discordjs/builders";
 
 export const data = {
   name: "avatar",
   description: "Kullanıcının avatarını döndürür",
   execute(interaction) {
-    const { embed } = interaction.client;
-    const target = interaction.options._hoistedOptions?.[0]?.member;
+    const target =
+      interaction.options._hoistedOptions?.[0]?.member || interaction.member;
+    const avatar = target.displayAvatarURL({ dynamic: true, size: 512 });
+
+    const responseEmbed = new EmbedBuilder()
+      .setTitle(`${target.displayName} Adlı Kullanıcının Avatarı`)
+      .setColor("DarkNavy")
+      .setDescription(
+        `[Buraya](${avatar}) Tıklayarak Avatar Bağlantısına ulaşabilirsiniz`
+      )
+      .setImage(avatar);
+    interaction.reply({ embeds: [responseEmbed] });
   },
 };
 
-export const slash_data = new SlashCommandBuilder()
-  .setName(data.name)
-  .setDescription(data.description)
-  .addUserOption((option) =>
-    option.setName("kullanıcı").setDescription("The user").setRequired(true)
-  );
+export const slash_data = {
+  name: data.name,
+  description: data.description,
+  options: [
+    {
+      name: "kullanıcı",
+      description: "Kullanıcı adını giriniz",
+      type: 6,
+      required: true,
+    },
+  ],
+};
+
+// export const slash_data = new SlashCommandBuilder()
+//   .setName(data.name)
+//   .setDescription(data.description)
+//   .addUserOption((option) =>
+//     option
+//       .setName("kullanıcı")
+//       .setDescription("Kullanıcı adı giriniz")
+//       .setRequired(true)
+//   );
+// *****************************************************
 //   .addSubcommand((subcommand) =>
 //     subcommand.addUserOption((option) =>
 //       option.setName("Kullanıcı").setDescription("Lütfen kullanıcıyı giriniz")
