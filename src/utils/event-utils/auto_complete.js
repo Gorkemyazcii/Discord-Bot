c8a004924403f48b1dd744528f9f922a57a375ec
Görkem <Gkrem33@gmail.com>
2023-12-10 20:14:24 +0300

    Aranan komut input kısmına yazıldığı anda komut ismine göre öneride bulunması

---
+++ src/utils/event-utils/auto_complete.js
@@ -0,0 +1,13 @@
+export default (interaction) => {
+  if (interaction.commandName == "help") {
+    const focusedValue = interaction.options.getFocused();
+
+    const choices = Array.from(interaction.client.commands.keys());
+
+    const filtered = choices.filter((choice) =>
+      choice.startsWith(focusedValue)
+    );
+    const result = filtered.map((choice) => ({ name: choice, value: choice }));
+    interaction.respond(result);
+  }
+};
