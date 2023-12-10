07b92dcb31514022e1f1e0f26e343fe94f2830f3
Görkem <Gkrem33@gmail.com>
2023-12-10 20:11:05 +0300

    Sahip olunan komutları bir embed şeklinde döndüren komut

--- src/commands/Bot/help.js
+++ src/commands/Bot/help.js
@@ -1,13 +1,52 @@
-import { EmbedBuilder } from "discord.js";
 import { SlashCommandBuilder } from "@discordjs/builders";
+import { EmbedBuilder } from "discord.js";
 export const data = {
   name: "help",
-  description: "Komutları gösterir",
+  description: "Yardım mesajı gönderir",
   execute(interaction) {
-    interaction.reply(" `!delete` `!ban` `!ping` ```Şimdilik olanlar```");
+    const { embed, commands } = interaction.client;
+
+    const commandName = interaction?.options?._hoistedOptions[0]?.value;
+
+    if (!commands.has(commandName)) {
+      return interaction.reply({
+        embeds: [embed(`\`${commandName}\` Adlı komut bulunamadı`, "Red")],
+      });
+    }
+
+    const command = commands.get(commandName).data;
+
+    const response = new EmbedBuilder().setColor("Navy").addFields(
+      { name: "Komut adı", value: `${command.name}`, inline: true },
+      { name: "\u200b", value: "\u200b", inline: true },
+      {
+        name: "Cooldown",
+        value: `${command.cooldown || 5} Saniye`,
+        inline: true,
+      },
+      { name: "Açıklama", value: `${command.description}` }
+    );
+    interaction.reply({ embeds: [response] });
   },
 };
 
-export const slash_data = new SlashCommandBuilder()
-  .setName(data.name)
-  .setDescription(data.description);
+export const slash_data = {
+  name: data.name,
+  description: data.description,
+  options: [
+    {
+      name: "komut_adı",
+      description: "Lütfen Bilgi almak istediğiniz komutun adını giriniz",
+      type: 3,
+    },
+  ],
+};
+// export const slash_data = new SlashCommandBuilder()
+//   .setName(data.name)
+//   .setDescription(data.description)
+//   .addStringOption((option) =>
+//     option
+//       .setName("komut_adı")
+//       .setDescription("Lütfen Bilgi almak istediğiniz komutun adını giriniz")
+//       .setAutocomplete(true)
+//   );
