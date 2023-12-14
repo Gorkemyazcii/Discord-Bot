c4197e58186095e689622dcf2dbc6b302af187e5
Görkem <Gkrem33@gmail.com>
2023-12-14 18:24:38 +0300

    Ping komutu için türkçe dil desteği yapıldı

--- src/commands/Bot/ping.js
+++ src/commands/Bot/ping.js
@@ -1,25 +1,30 @@
 import { EmbedBuilder } from "discord.js";
+import { t } from "i18next";
 
 export const data = {
-  name: "ping",
-  description: "Botun ve Discord'un gecikmesini Gönderir",
-
+  name: t("ping.name"),
+  description: t("ping.description"),
   execute(interaction) {
     const { emoji, ws } = interaction.client;
-    console.log(interaction);
 
     const dicord_ping = ws.ping;
     const bot_ping = Date.now() - interaction.createdTimestamp;
 
     const response = new EmbedBuilder().setColor("Random").addFields(
       {
-        name: `${emoji("discord_ico74")} Discord ping `,
+        name: `${emoji("discord_ico74")} ${t("ping.discord_latency", {
+          lng: interaction.locale,
+          ns: "commands",
+        })}`,
         value: `${dicord_ping} ms `,
         inline: true,
       },
       { name: "\u200b", value: "\u200b", inline: true },
       {
-        name: `${emoji("BOT")} Bot ping`,
+        name: `${emoji("BOT")} ${t("ping.bot_latency", {
+          lng: interaction.locale,
+          ns: "commands",
+        })}`,
         value: `${bot_ping} ms`,
         inline: true,
       }
@@ -31,6 +36,12 @@ export const data = {
 export const slash_data = {
   name: data.name,
   description: data.description,
+  name_localizations: {
+    tr: t("ping.name", { lng: "tr" }),
+  },
+  description_localizations: {
+    tr: t("ping.description", { lng: "tr" }),
+  },
 };
 
 // export const slash_data = new SlashCommandBuilder()
