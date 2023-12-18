dfc1641e6841d13e450b0ba4046a05cbbffd387d
Görkem <Gkrem33@gmail.com>
2023-12-18 16:50:16 +0300

    Database denemeleri yapıldı

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,8 +1,12 @@
 import register_commands from "../utils/test/register_commands.js";
+import { fetch, update } from "../utils/database/mongoose_methods.js";
 
 export default (client) => {
-  client.once("ready", () => {
+  client.once("ready", async () => {
     console.log("Test Bot hazır");
-    register_commands(client, "global");
+    // register_commands(client, "global");
+    console.log(await fetch("123"));
+    await update("123", { $set: { test: "gorkem" } });
+    console.log(await fetch("123"));
   });
 };
