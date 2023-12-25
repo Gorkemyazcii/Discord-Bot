ecd41d1512cf76c14c80d0c051d2489a13f60e24
Görkem <Gkrem33@gmail.com>
2023-12-25 14:25:24 +0300

    finish

--- src/commands/music/jump.js
+++ src/commands/music/jump.js
@@ -1,6 +1,7 @@
 import { ChannelType } from "discord.js";
 import { EmbedBuilder } from "discord.js";
 import client from "../../app.js";
+import { t } from "i18next";
 export const data = {
   name: "jump",
   description: "Numarasını girdiğiniz şarkıya gider",
@@ -17,19 +18,29 @@ export const data = {
     const voiceChannel = member.voice.channel;
 
     if (!voiceChannel)
-      return interaction.followUp({
-        content: "You must be in a voice channel to execute music commands.",
+      return interaction.reply({
+        content: t("voiceChannel", {
+          ns: "common",
+          lng: interaction.locale,
+        }),
       });
     try {
       const queue = await client.distube.getQueue(voiceChannel);
 
       if (!queue) {
-        embed.setColor("Red").setDescription("There is no active queue.");
+        embed.setColor("Red").setDescription(
+          t("no_queue", {
+            ns: "common",
+            lng: interaction.locale,
+          })
+        );
         return interaction.reply({ embeds: [embed], ephemeral: true });
       }
 
       await queue.jump(number - 1);
-      embed.setColor("Blue").setDescription(`⏩ Forwarded the song for `);
+      embed
+        .setColor("Blue")
+        .setDescription(t("jump.message", { lng: interaction.locale }));
       return interaction.reply({ embeds: [embed], ephemeral: true });
     } catch (err) {
       console.log(err);
@@ -39,10 +50,19 @@ export const data = {
 export const slash_data = {
   name: data.name,
   description: data.description,
+  description_localizations: {
+    tr: t("jump.description", { lng: "tr" }),
+  },
   options: [
     {
-      name: "index",
-      description: "gitmek istediğiniz müzik numarasını giriniz",
+      name: t("jump.jump_option.name"),
+      description: t("jump.jump_option.description"),
+      name_localizations: {
+        tr: t("jump.jump_option.name", { lng: "tr" }),
+      },
+      description_localizations: {
+        tr: t("jump.jump_option.description", { lng: "tr" }),
+      },
       type: 4,
       required: true,
       min_value: 0,
